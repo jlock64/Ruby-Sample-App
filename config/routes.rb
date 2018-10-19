793939c693800e30c46d260dd1e67e4aa1519e3a
Justin Lock <jlock@TRHC.com>
2018-10-18 20:53:50 -0400

    add users controller and views and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root :to => 'static_pages#home'
   get '/help' => 'static_pages#help'
 
   # you can also use the to: keyword
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
