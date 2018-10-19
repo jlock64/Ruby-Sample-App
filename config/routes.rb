081af4fae48f35687a6128dffc151a685b168d18
Justin Lock <jlock@TRHC.com>
2018-10-18 20:03:15 -0400

    add partials and more tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  root :to => 'static_pages#home'
+  get '/help' => 'static_pages#help'
+
+  # you can also use the to: keyword
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
